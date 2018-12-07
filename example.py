8904698a9d1327174169360a986afa9909b98a3b
José Coelho <josecoelho96@gmail.com>
2018-12-07 15:07:16 +0000

    Clean example

--- example.py
+++ example.py
@@ -2,7 +2,6 @@ import csp
 
 class Problem(csp.CSP):
 
-
     def constraints_function(self, A, a, B, b):
         if A == "X" and B == "Y":
             return a < b
@@ -15,14 +14,11 @@ class Problem(csp.CSP):
         elif A == "Z" and B == "Y":
             return a == b
         elif A == "Z" and B == "T":
-            # return a >= b
             return a > b
         elif A == "T" and B == "Z":
-            # return a <= b
             return a < b
         elif A == "T" and B == "X":
             return a > b
-
 
     def __init__(self):
         variables = {"X", "Y", "Z", "T"}
@@ -41,17 +37,9 @@ class Problem(csp.CSP):
 
         super().__init__(variables, domains, graph, self.constraints_function)
 
-    def dump_solution(self, fh):
-        # Place here your code to write solution to opened file object fh
-        pass
-
 def solve():
     p = Problem()
-    # Place here your code that calls function csp.backtracking_search(self, ...)
     print(csp.backtracking_search(p))
-
-
-    # p.dump_solution(output_file)
 
 if __name__ == "__main__":
     solve()
