7befe0880fe8975b303ed1ad96e0a1ae30fd1799
David Teles <contacto@davidteles.com>
2018-12-09 15:58:56 +0000

    New giant map added

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from school_schedule_csp import solve
 # input_file_path = "inputs/public_test_2.txt"
 # output_file_path = "outputs/public_test_2.txt"
 
-input_file_path = "inputs/public_test_3.txt"
-output_file_path = "outputs/public_test_3.txt"
+input_file_path = "inputs/Giant.txt"
+output_file_path = "outputs/Giant.txt"
 
 input_file = open(input_file_path, "r")
 output_file = open(output_file_path, "w+")
