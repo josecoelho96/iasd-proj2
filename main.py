c21b9467dff0516eee11b96ca99569d895a73d29
José Coelho <josecoelho96@gmail.com>
2018-12-09 01:38:31 +0000

    Run public tests

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from school_schedule_csp import solve
 
-input_file_path = "inputs/example.txt"
-output_file_path = "outputs/example.txt"
+# input_file_path = "inputs/example.txt"
+# output_file_path = "outputs/example.txt"
+
+# input_file_path = "inputs/public_test_1.txt"
+# output_file_path = "outputs/public_test_1.txt"
+
+# input_file_path = "inputs/public_test_2.txt"
+# output_file_path = "outputs/public_test_2.txt"
+
+input_file_path = "inputs/public_test_3.txt"
+output_file_path = "outputs/public_test_3.txt"
 
 input_file = open(input_file_path, "r")
 output_file = open(output_file_path, "w+")
