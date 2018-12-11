0e77d6204e721764fdc19bd962df46827aedb720
José Coelho <josecoelho96@gmail.com>
2018-12-11 15:46:41 +0000

    Add code for submission 4

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from school_schedule_csp import solve
 # input_file_path = "inputs/example2.txt"
 # output_file_path = "outputs/example2.txt"
 
-input_file_path = "inputs/public_test_1.txt"
-output_file_path = "outputs/public_test_1.txt"
+# input_file_path = "inputs/public_test_1.txt"
+# output_file_path = "outputs/public_test_1.txt"
 
 # input_file_path = "inputs/public_test_2.txt"
 # output_file_path = "outputs/public_test_2.txt"
@@ -18,8 +18,8 @@ output_file_path = "outputs/public_test_1.txt"
 # input_file_path = "inputs/Test2.txt"
 # output_file_path = "outputs/Test2.txt"
 
-# input_file_path = "inputs/Test3.txt"
-# output_file_path = "outputs/Test3.txt"
+input_file_path = "inputs/Test3.txt"
+output_file_path = "outputs/Test3.txt"
 
 # input_file_path = "inputs/Test4.txt"
 # output_file_path = "outputs/Test4.txt"
--- main_submission_4.py
+++ main_submission_4.py
@@ -96,7 +96,6 @@ class Problem(csp.CSP):
         new_solution = csp.backtracking_search(self,
             select_unassigned_variable = csp.mrv,
             inference = csp.forward_checking)
-        print("[D] [{}] new solution : {}".format(self.current_upper_bound, new_solution))
 
         if new_solution != None:
             self.solution = new_solution
@@ -116,10 +115,8 @@ class Problem(csp.CSP):
     def dump_solution(self, fh):
         if self.solution == None:
             fh.write("None")
-            print("[DEBUG] : None")
         else:
             for key, value in self.solution.items():
-                print("[DEBUG] : {} {},{} {}".format(key, value[0][0], value[0][1], value[1]))
                 fh.write("{} {},{} {}\n".format(key, value[0][0], value[0][1], value[1]))
 
 def solve(input_file, output_file):
